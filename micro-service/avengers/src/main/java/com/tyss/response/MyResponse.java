4d33e86e011a79cdd1717066d7fa499cf27e2062
Anil Thapa <anilsthapa07@gmail.com>
2022-11-21 10:34:05 +0530

    micro services and redis cache done

--- micro-service/avengers/src/main/java/com/tyss/response/MyResponse.java
+++ micro-service/avengers/src/main/java/com/tyss/response/MyResponse.java
@@ -1,12 +1,19 @@
 package com.tyss.response;
 
+import java.io.Serializable;
+
 import lombok.Builder;
 import lombok.Data;
 
 @Data
 @Builder
-public class MyResponse {
+public class MyResponse implements Serializable {
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 6568726203999776289L;
+
 	private String message;
 	
 	private Object data;
